7b3d9a84023f116587761c421ce247a967e2eec7
Belen-Eliza <beluella1@gmail.com>
2024-11-26 14:47:10 -0300

    validación de mails repetidos

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -41,6 +41,15 @@ const UserRoute = (prisma: PrismaClient)=>{
   router.post('/signup', async (req, res) => {
     const { name, mail, password } = req.body;
     const mail_normalizado =mail.toLowerCase();
+    const ya_existe = await prisma.user.findUnique({
+      where: {
+        mail: mail_normalizado
+      }
+    })
+    if (ya_existe) {
+      res.status(409).send("El usuario ya existe")
+      return 
+    }
     try {
       const result = await prisma.user.create({
         data: {
@@ -61,6 +70,15 @@ const UserRoute = (prisma: PrismaClient)=>{
     let { new_name, new_mail, new_password } = req.body; //no mandar atributos a no actualizar, o chequear
     if (new_mail) {
       new_mail= new_mail.toLowerCase();
+    }
+    const ya_existe = await prisma.user.findUnique({
+      where: {
+        mail: new_mail
+      }
+    })
+    if (ya_existe) {
+      res.status(409).send("Ya existe un usuario con ese mail")
+      return 
     }
     const result = await prisma.user.update({
         data: {
