a8c007445860e88f4728f574f778b425d11b1a23
Belen-Eliza <beluella1@gmail.com>
2025-01-25 11:24:10 -0300

    empezando con edición de presupuestos

--- src/routes/presupuesto.routes.ts
+++ src/routes/presupuesto.routes.ts
@@ -103,6 +103,28 @@ const PresupuestoRoute = (prisma: PrismaClient) => {
     }
   });
 
+  router.get("/activos/:user_id", async (req, res) => {
+    const { user_id } = req.params;
+
+    try {
+      const presupuestos = await prisma.presupuesto.findMany({
+        where: { user_id: Number(user_id), activo: 1 },
+      });
+
+      if (presupuestos.length === 0) {
+        res.status(404).send({
+          message: "No se encontraron presupuestos para este usuario.",
+        });
+        return;
+      }
+
+      res.json(presupuestos);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ message: "Error al obtener los presupuestos." });
+    }
+  });
+
   router.get("/unico/:presupuesto_id",async(req,res)=>{
     const { presupuesto_id } = req.params;
     const presupuesto = await prisma.presupuesto.findUnique({
@@ -111,6 +133,25 @@ const PresupuestoRoute = (prisma: PrismaClient) => {
       }
     });
     res.json(presupuesto)
+  });
+
+  router.patch("/:presupuesto_id",async(req,res)=>{
+    const {presupuesto_id} =req.params;
+    const { new_desc, new_amount, new_act, new_date } = req.body;
+
+    const result = await prisma.presupuesto.update({
+      data: {
+          descripcion: new_desc,
+          fecha_objetivo: new_date,
+          montoTotal: new_amount,
+          activo: new_act
+      },
+      where: {
+          id: parseInt(presupuesto_id)
+      },
+  })
+
+    res.json(result);
   })
 
   return router;
